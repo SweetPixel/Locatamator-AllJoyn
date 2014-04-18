ef01ae2446a21fa79e1614fb038d299c2fe9cce4
aliirz <dejavu87@gmail.com>
2014-04-18 17:23:29 +0500

    Initial Files

---
+++ Locatamator-Alljoyn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Locatamator-Alljoyn
+//
+//  Created by Hammad Mehmood on 11/04/2014.
+//  Copyright (c) 2014 Sweet Pixel Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
