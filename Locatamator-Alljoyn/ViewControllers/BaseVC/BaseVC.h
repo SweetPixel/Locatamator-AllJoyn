6f4de0de385e0f5008dca23a0f43cd583e533f9e
ijameelkhan <i.jameelkhan@gmail.com>
2014-04-29 13:42:34 +0500

    Project directories added

---
+++ Locatamator-Alljoyn/ViewControllers/BaseVC/BaseVC.h
@@ -0,0 +1,13 @@
+//
+//  BaseVC.h
+//  Locatamator-Alljoyn
+//
+//  Created by Hammad Mehmood on 29/04/2014.
+//  Copyright (c) 2014 Sweet Pixel Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseVC : UIViewController
+
+@end
---
+++ Locatamator-Alljoyn/ViewControllers/BaseVC/BaseVC.m
@@ -0,0 +1,49 @@
+//
+//  BaseVC.m
+//  Locatamator-Alljoyn
+//
+//  Created by Hammad Mehmood on 29/04/2014.
+//  Copyright (c) 2014 Sweet Pixel Studios. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface BaseVC ()
+
+@end
+
+@implementation BaseVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
